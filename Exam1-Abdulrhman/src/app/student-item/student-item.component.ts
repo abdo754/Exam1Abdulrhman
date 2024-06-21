bfc9c69bb4b71bb98807261234454282d15b9ca8
abdo754 <123410883+abdo754@users.noreply.github.com>
2024-06-21 18:42:47 -0400

    the componets
    Create the components  and service for the students

---
+++ Exam1-Abdulrhman/src/app/student-item/student-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-item.component.html',
+  styleUrl: './student-item.component.scss'
+})
+export class StudentItemComponent {
+
+}
